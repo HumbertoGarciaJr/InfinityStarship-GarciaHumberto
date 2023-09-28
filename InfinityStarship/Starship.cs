c0e05947a6d9e0418295c8ebef6ca74753377ab7
Humberto Garcia Jr <humberto.garcia3334@gmail.com>
2023-09-28 03:54:45 -0500

    Continue 'Starship' class. Add namespaces(System.Drawing) to 'Starship' class. Add spawnSpaceship Method to build and Spawn new PictureBox as a spaceship(Starship) object. Work on spawnSpaceship Method and give spaceship object Properties Constructor. Work on spawnSpaceship Method to make spaceship PictureBox as a available control to the 'playScreenForm' form, and calling it in Init Method which is called in Starship Constructor.

--- InfinityStarship/Starship.cs
+++ InfinityStarship/Starship.cs
@@ -3,10 +3,11 @@
 
 using System;
 using System.Collections.Generic;
+using System.Drawing;//Using namespace System.Drawing to allow use of Drawing controls/properties
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;//namespace System.Windows.Forms allows use of Windows.Forms controls/properties
+using System.Windows.Forms;//Using namespace System.Windows.Forms allows use of Windows.Forms controls/properties
 
 //Project/Namespace InfinityStarship
 namespace InfinityStarship
@@ -15,7 +16,7 @@ namespace InfinityStarship
     class Starship
     {
         //Objects
-        //Instantiations/Attributes Properties
+        //Instantiations/Attributes Properties Functions(Declarations)
         public PictureBox spaceship;
         public int spaceshipSpeed { get; set; }
         public playScreenForm form;
@@ -23,10 +24,9 @@ namespace InfinityStarship
         //Constructor
         public Starship (playScreenForm form)//1 parameter
         {
-            //Attribute Initializations
+            //Objects/Attributes Initializations
             this.form = form;//store form(parameter) into this(Starship) form
             spaceshipSpeed = 5;//store 5 into spaceshipSpeed
-            spaceship = new PictureBox();//store new class PictureBox into spaceship object
 
             //Call Function Init
             Init();
@@ -35,7 +35,28 @@ namespace InfinityStarship
         //Function Init to hold all methods within one
         private void Init()
         {
-            //TODO
+            //Call Function spawnSpaceship
+            spawnSpaceship();
         }
+
+        //Function spawnSpaceship to build and Spawn spaceship(Starship)
+        private void spawnSpaceship()
+        {
+            //Store new class PictureBox into spaceship object
+            spaceship = new PictureBox()
+            {//spaceship Properties Constructor
+                BackColor = Color.Black,//Temporary PictureBox Backcolor of Black
+                Width = 100,//PictureBox Width of 100
+                Height = 75,//PictureBox Height of 75
+                Visible = true,//PictureBox Visibility at true
+                Left = (form.ClientSize.Width - 100) / 2,//PictureBox at Left of ClientSize form - 100 / 2(Position Middle Horizontally)
+                Top = (form.ClientSize.Height - 100)//PictureBox at Top of ClientSize form - 100(Position Bottom Vertically)
+            };
+
+            //Call Function Add to Property of form(playScreenForm)
+            form.Controls.Add(spaceship);//adding spaceship PictureBox as a available control to the form(frmArkanoid)
+        }
+
+        //TODO
     }
 }
