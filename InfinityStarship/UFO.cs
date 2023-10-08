63d66a350d9325702fec206eee949595a2cde352
Humberto Garcia Jr <humberto.garcia3334@gmail.com>
2023-10-07 20:57:53 -0500

    Continue 'UFO' class. Work on UFO constructor and initialize enemies collection list. Work on spawnEnemyWave Method and give enemies list Properties Constructor. Work on spawnEnemyWave Method to make enemies list PictureBoxes as a available controls to the 'playScreenForm' form.

--- InfinityStarship/UFO.cs
+++ InfinityStarship/UFO.cs
@@ -31,6 +31,7 @@ namespace InfinityStarship
             enemySpeedX = 5;//store 5 into enemySpeedX(5 pixels per tick)
             enemySpeedY = 5;//store 5 into enemySpeedY(5 pixels per tick)
             increasedSpeed = 0;//store 0 into increasedSpeed(default speed should not be increased)
+            enemies = new List<PictureBox>();//store new class PictureBox obj collection List into enemies
 
             //Call Function Init
             Init();
@@ -46,6 +47,19 @@ namespace InfinityStarship
         //Function spawnEnemyWave to build and spawn enemies(UFOs)
         private void spawnEnemyWave()
         {
+            //Call Function Add of enemies(To store new class PictureBox into enemies list)
+            enemies.Add(new PictureBox()
+            {//enemies list Properties Constructor
+                BackColor = Color.ForestGreen,//Temporary PictureBox Backcolor of ForestGreen
+                Width = 75,//PictureBox Width of 75
+                Height = 75,//PictureBox Height of 75
+                Visible = true,//PictureBox Visibility at true
+                Left = 300,//PictureBox at Left of 300(Default)
+                Top = 100,//PictureBox at Top of 100(Default)
+            });
+
+            //Call Function Add to Property of form(playScreenForm)
+            form.Controls.Add(enemies[0]);//adding enemy temporary index to enemies list of PictureBoxes as a available controls to the form(playScreenForm)
             //TODO
         }
     }
