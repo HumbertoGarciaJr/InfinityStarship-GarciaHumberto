71d2e808aa64367059d548b9c84ad6c7cda6fb8e
Humberto Garcia Jr <humberto.garcia3334@gmail.com>
2023-10-19 14:31:52 -0500

    Start 'Laser' class. Create 'UFO' class, and basic setup. Add Instantiations and Attributes Properties to 'Laser' class. Add namespaces(System.Windows.Forms) to 'Laser' class. Add constructor setup with 'playScreenForm' form. Add Init Method to hold all future methods within it to 'Laser' class.

---
+++ InfinityStarship/Laser.cs
@@ -0,0 +1,34 @@
+﻿/* Description: Laser Class focusing on games shooting system, Author: Humberto Garcia */
+//CLASS IMPLEMENTATION FILE
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;//Using namespace System.Windows.Forms allows use of Windows.Forms controls/properties
+
+//Project/Namespace Infinity Starship
+namespace InfinityStarship
+{
+    //Class Laser
+    class Laser
+    {
+        //Objects Instantiation & Attribute Properties Function(Declarations)
+        public PictureBox laser;
+        public int laserSpeed { get; set; }
+        public playScreenForm form;
+
+        //Constructor
+        public Laser (playScreenForm form)//1 parameter
+        {
+            //TODO
+        }
+
+        //Function Init to hold all methods within one
+        private void Init()
+        {
+
+        }
+    }
+}
