7833e281a395d0af96fd73f11a3f0964e6d6cbb4
Humberto Garcia Jr <humberto.garcia3334@gmail.com>
2023-10-20 18:57:34 -0500

    Continue 'Laser' class. Add namespaces(System.Drawing) to 'Laser' class. Work on Laser constructor and initialize some of its main objects/attributes. Work on Laser constructor including Init Method in it to call it. Work on Init Method and give laser object Properties Constructor. Work on Init Method to make laser PictureBox as a available control to the 'playScreenForm' form. Add lasers List collection to declare and initialize in 'Laser' class.

--- InfinityStarship/Laser.cs
+++ InfinityStarship/Laser.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Drawing;//Using namespace System.Drawing to allow use of Drawing controls/properties
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,17 +19,37 @@ namespace InfinityStarship
         public PictureBox laser;
         public int laserSpeed { get; set; }
         public playScreenForm form;
+        public List<PictureBox> lasers;
 
         //Constructor
         public Laser (playScreenForm form)//1 parameter
         {
-            //TODO
+            this.form = form;//store form(parameter) into this(Laser) form
+            laserSpeed = 5;//store 5 into laserSpeed
+            lasers = new List<PictureBox>();//store new class PictureBox obj collection List into lasers
+
+            //Call Function Init
+            Init();
         }
 
         //Function Init to hold all methods within one
         private void Init()
         {
+            //Store new class PictureBox into laser object
+            laser = new PictureBox()
+            {//laser Properties Constructor
+                BackColor = Color.Red,//PictureBox Backcolor of Red
+                Width = 10,//PictureBox Width of 10
+                Height = 50,//PictureBox Height of 50
+                Visible = false,//Temporary PictureBox Visibility at true
+                Left = (form.ClientSize.Width - 10) / 2,//Temporary PictureBox at Left of ClientSize form - 10 / 2(Position Middle Horizontally)
+                Top = (form.ClientSize.Height - 150)//Temporary PictureBox at Top of ClientSize form - 150(Position Bottom Vertically)
+            };
 
+            //Call Function Add to Property of form(playScreenForm)
+            form.Controls.Add(laser);//adding laser PictureBox as a available control to the form(frmArkanoid)
         }
+
+        //TODO
     }
 }
