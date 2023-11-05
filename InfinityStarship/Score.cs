73946b79a132a94814ac0abd9b1e1ae1a4e7653c
Humberto Garcia Jr <humberto.garcia3334@gmail.com>
2023-11-04 20:51:27 -0500

    Start 'Score' class. Create 'Score' class, and basic setup. Add Static(Global) Attribute Properties to 'Score' class to allow scoring system and its rules to be accessed globally by the rest of the game.

---
+++ InfinityStarship/Score.cs
@@ -0,0 +1,22 @@
+﻿/* Description: Score Class focusing on games scoring system and its rules accessed globally by the rest of the game, Author: Humberto Garcia */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Project/Namespace Infinity Starship
+namespace InfinityStarship
+{
+    //Class Score
+    class Score
+    {
+        //Static(Global) Attribute Properties Function(Declarations)
+        public static int totalScore { get; set; }
+        public static int getScore { get { return totalScore; } }
+        public static bool gameOver { get; set; }
+
+        //TODO
+    }
+}
