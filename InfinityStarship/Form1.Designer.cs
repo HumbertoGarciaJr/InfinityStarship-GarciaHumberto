cd78bd6f5a77cde4fffa866040b79bf9f82a527f
Humberto Garcia Jr <humberto.garcia3334@gmail.com>
2023-11-09 15:18:48 -0600

    Continue 'playScreenForm' form. Work on design of the 'playScreenForm' and adjust its in project size to be 1100w by 600h. Work on design and add label control named "label_score" to Visually display current score in 'playScreenForm'. Work on design and add label control named "label_title" to Visually display the title of game.

--- InfinityStarship/Form1.Designer.cs
+++ InfinityStarship/Form1.Designer.cs
@@ -34,6 +34,8 @@ namespace InfinityStarship
             this.timer_playScreenForm = new System.Windows.Forms.Timer(this.components);
             this.imageList_enemies = new System.Windows.Forms.ImageList(this.components);
             this.pictureBox_spaceship = new System.Windows.Forms.PictureBox();
+            this.label_score = new System.Windows.Forms.Label();
+            this.label_title = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_spaceship)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,19 +54,46 @@ namespace InfinityStarship
             // pictureBox_spaceship
             // 
             this.pictureBox_spaceship.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_spaceship.Image")));
-            this.pictureBox_spaceship.Location = new System.Drawing.Point(350, 365);
+            this.pictureBox_spaceship.Location = new System.Drawing.Point(492, 470);
             this.pictureBox_spaceship.Name = "pictureBox_spaceship";
             this.pictureBox_spaceship.Size = new System.Drawing.Size(100, 75);
             this.pictureBox_spaceship.TabIndex = 0;
             this.pictureBox_spaceship.TabStop = false;
             this.pictureBox_spaceship.Visible = false;
             // 
+            // label_score
+            // 
+            this.label_score.AutoSize = true;
+            this.label_score.BackColor = System.Drawing.Color.Transparent;
+            this.label_score.Font = new System.Drawing.Font("Nachlieli CLM", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label_score.ForeColor = System.Drawing.Color.White;
+            this.label_score.Location = new System.Drawing.Point(20, 20);
+            this.label_score.Name = "label_score";
+            this.label_score.Size = new System.Drawing.Size(191, 52);
+            this.label_score.TabIndex = 1;
+            this.label_score.Text = "SCORE:";
+            // 
+            // label_title
+            // 
+            this.label_title.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label_title.AutoSize = true;
+            this.label_title.BackColor = System.Drawing.Color.Transparent;
+            this.label_title.Font = new System.Drawing.Font("Nachlieli CLM", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label_title.ForeColor = System.Drawing.Color.White;
+            this.label_title.Location = new System.Drawing.Point(295, 20);
+            this.label_title.Name = "label_title";
+            this.label_title.Size = new System.Drawing.Size(495, 59);
+            this.label_title.TabIndex = 2;
+            this.label_title.Text = "INFINITY STARSHIP";
+            // 
             // playScreenForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1084, 561);
+            this.Controls.Add(this.label_title);
+            this.Controls.Add(this.label_score);
             this.Controls.Add(this.pictureBox_spaceship);
             this.Name = "playScreenForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -74,6 +103,7 @@ namespace InfinityStarship
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.playScreenForm_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_spaceship)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,6 +112,8 @@ namespace InfinityStarship
         private System.Windows.Forms.Timer timer_playScreenForm;
         public System.Windows.Forms.ImageList imageList_enemies;
         public System.Windows.Forms.PictureBox pictureBox_spaceship;
+        private System.Windows.Forms.Label label_score;
+        private System.Windows.Forms.Label label_title;
     }
 }
 
